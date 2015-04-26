c2d45b14f8815cbdf5ed3d65cf4e826c093c8284
Héctor Tortosa <hectortosa@icloud.com>
2015-04-27 00:10:53 +0200

    Added org statistics and rebranding

--- js/U4App.js
+++ js/U4App.js
@@ -7,6 +7,14 @@ angular
 				templateUrl: 'js/app.tpl.html',
 				restrict: 'E',
 				link: function(scope) {
+					GithubService.getOrgInfo('Unit4').then(function(org) {
+						scope.org = org;
+					});
+
+					GithubService.getOrgPublicMembers('Unit4').then(function(members) {
+						scope.publicMembers = members;
+					});
+
 					GithubService.getOrgRepos('Unit4').then(function(repos) {
 						scope.repos = repos;
 					});
--- js/services/GitHubService.js
+++ js/services/GitHubService.js
@@ -6,6 +6,20 @@ angular
 				org: '@org',
 				callback: 'JSON_CALLBACK'
 			}, {
+				info: {
+					method: 'JSONP',
+					url: 'https://api.github.com/orgs/:org',
+					headers: {
+						'Accept': 'application/vnd.github.v3+json'
+					}
+				},
+				publicMembers: {
+					method: 'JSONP',
+					url: 'https://api.github.com/orgs/:org/public_members',
+					headers: {
+						'Accept': 'application/vnd.github.v3+json'
+					}
+				},
 				repos: {
 					method: 'JSONP',
 					url: 'https://api.github.com/orgs/:org/repos',
@@ -14,6 +28,28 @@ angular
 					}
 				}
 			});
+
+			this.getOrgInfo = function(name) {
+				return organizations.info({
+					org: name
+				}).$promise.then(function(response) {
+					return response.data;
+				}).catch(function(e){
+					console.log(e);
+					return [];
+				});
+			};
+
+			this.getOrgPublicMembers = function(name) {
+				return organizations.publicMembers({
+					org: name
+				}).$promise.then(function(response) {
+					return response.data;
+				}).catch(function(e){
+					console.log(e);
+					return [];
+				});
+			};
 
 			this.getOrgRepos = function(name) {
 				return organizations.repos({
